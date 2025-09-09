daa9032ca2338cdba78252a262125263aa821e3a
Alyssa <ashinnickmitchell@gmail.com>
2025-09-08 18:02:23 -0600

    pacman - initial setup

---
+++ src/projects/Pacman.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Pacman() {
+  return (
+    <main className="pacman-container">
+      <div className="pacman"></div>
+      <div className="pellet"></div>
+      <div className="pellet"></div>
+      <div className="pellet"></div>
+    </main>
+  );
+}
