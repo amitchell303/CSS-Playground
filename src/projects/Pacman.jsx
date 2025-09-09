3df1e7304094a3073425e987261e8efa53a5ec7f
Alyssa <ashinnickmitchell@gmail.com>
2025-09-08 18:24:38 -0600

    pacman - initial ghost figure

--- src/projects/Pacman.jsx
+++ src/projects/Pacman.jsx
@@ -8,6 +8,14 @@ export default function Pacman() {
       <div className="pellet"></div>
       <div className="pellet"></div>
       <div className="pellet"></div>
+      <div className="ghost-container">
+        <div className="ghost-top"></div>
+        <div className="ghost-bottom-container">
+          <div className="ghost-bottom"></div>
+          <div className="ghost-bottom"></div>
+          <div className="ghost-bottom"></div>
+        </div>
+      </div>
     </main>
   );
 }
